9b4c37e5dd16286713e9e931d017c67374e297ac
kavyamanu <kavya.manupati@babylonhealth.com>
2022-06-04 10:21:41 +0530

    handel get request

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-})
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
-app.get("/api/courses/:id", (req,res)=> {
-    res.send(req.params)
-})
-const port = process.env.PORT || 3000
+app.get("/api/courses/:id", (req, res) => {
+  let course = courses.find((course) => course.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("page not found....");
+  res.send(course);
+});
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening to port ${port}...`);
 });
